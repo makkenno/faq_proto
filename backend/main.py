f9e196fc2c355e4e0083b32ddc3c111a21952b28
makkenno <kota.m.2774@gmail.com>
2023-04-16 19:57:10 +0900

    ✨ ファイルアップロードと質問への回答を行うエンドポイントを作成

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from pdfminer.high_level import extract_text
+import pandas as pd
+import openai
+import langchain
 
 app = FastAPI()
 
@@ -11,3 +15,25 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/uploadfile/")
+async def upload_file(file: UploadFile = File(...)):
+    # PDFとCSVファイルの処理
+    content = ""
+    if file.filename.endswith(".pdf"):
+        content = extract_text(file.file)
+    elif file.filename.endswith(".csv"):
+        df = pd.read_csv(file.file)
+        content = df.to_string()
+
+    # ここでcontentを用いてFAQの抽出と回答の生成を行う
+
+    return {"filename": file.filename}
+
+
+@app.post("/ask/")
+async def ask_question(question: str):
+    # ここで質問に対して関連するFAQをデータベースから検索し、回答を生成する処理を行う
+
+    return {"answer": "回答のサンプル"}
